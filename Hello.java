2322619c3f734773e15fd946119dabaa23a5f3b0
Sheela08 <77281915+Sheela08@users.noreply.github.com>
2021-01-11 19:43:50 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package Hello;
 public class Hello
 {
 
-   public static void main(Strings[]args)
+   public static void main(String[] args)
    {
         
         System.out.println("Hello Java!!");
